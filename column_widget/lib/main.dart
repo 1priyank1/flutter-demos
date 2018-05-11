3a46511046ea246be3ffc90bbcad9f601ba297b1
Priyank <1priyank1@gmail.com>
2018-05-11 13:43:02 +0530

    column widget demo added

---
+++ column_widget/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      textDirection: TextDirection.ltr,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new Expanded(child : new Icon(Icons.sentiment_very_satisfied, textDirection: TextDirection.ltr, color: Color(0xFF00FF00),)),
+        new Expanded(child: new Text("One", textAlign: TextAlign.justify,textDirection: TextDirection.ltr)),
+        new Expanded(child: new Text("Two", textAlign: TextAlign.justify, textDirection: TextDirection.ltr)),
+        new Expanded(child: new Text("Three", textAlign: TextAlign.justify, textDirection: TextDirection.ltr)),
+        const FlutterLogo()
+      ],
+    );
+  }
+}
