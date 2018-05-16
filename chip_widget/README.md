91e8fe9707516b186d70c81264a91265cb7cfc5b
PRIYANK\Priyank <1priyank1@gmail.com>
2018-05-16 22:49:22 +0530

    Chip widget demo added

---
+++ chip_widget/README.md
@@ -0,0 +1,8 @@
+# chip_widget
+
+Chip widget demo app
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ chip_widget/lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Drawer Widget'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final List<ActorFilterEntry> _cast = <ActorFilterEntry>[
+    const ActorFilterEntry('India', 'I'),
+    const ActorFilterEntry('Australia', 'A'),
+    const ActorFilterEntry('Unites States', 'US'),
+    const ActorFilterEntry('United Kingdom', 'UK'),
+    const ActorFilterEntry('Caneda', 'C')
+  ];
+  List<String> _filters = <String>[];
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.subhead;
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+         child: new Column(
+           crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new Chip(
+                label: new Text('Chip'),
+                avatar: new CircleAvatar(
+                  backgroundColor: Colors.grey.shade800,
+                  child: new Text('C'),
+                ),
+            ),
+            new InputChip(
+                label: new Text("Input Chip"),
+                avatar: new CircleAvatar(
+                  backgroundColor: Colors.blue.shade700,
+                  child: new Text("IC"),
+                ),
+                onPressed: (){
+                  print("I am an Inpit chip.");
+                },
+            ),
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text('Filter Chip : ', style: textStyle),
+                new Wrap(
+                  children: actorWidgets.toList(),
+                ),
+                new Text('Selected Coutries: ${_filters.join(', ')}', style: textStyle ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Iterable<Widget> get actorWidgets sync* {
+    for (ActorFilterEntry actor in _cast) {
+      yield new Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: new FilterChip(
+          avatar: new CircleAvatar(child: new Text(actor.initials)),
+          label: new Text(actor.name),
+          selected: _filters.contains(actor.name),
+          onSelected: (bool value) {
+            setState(() {
+              if (value) {
+                _filters.add(actor.name);
+              } else {
+                _filters.removeWhere((String name) {
+                  return name == actor.name;
+                });
+              }
+            });
+          },
+        ),
+      );
+    }
+  }
+}
+
+
+class ActorFilterEntry {
+  const ActorFilterEntry(this.name, this.initials);
+  final String name;
+  final String initials;
+}
